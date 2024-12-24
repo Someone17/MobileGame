c7ecdfcd8c7f2137fa7a1f5db0423f443cb666ad
Someone17 <40706918+Someone17@users.noreply.github.com>
2024-12-23 21:17:51 -0300

    Add files via upload

---
+++ Assets/Level/LevelPieceBase.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelPieceBase : MonoBehaviour
+{
+    public Transform endPiece;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
