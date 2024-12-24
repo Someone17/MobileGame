f920ef616230df7c917c16a5412596f6eac92075
Someone17 <40706918+Someone17@users.noreply.github.com>
2024-12-23 21:20:26 -0300

    Add files via upload

---
+++ Assets/ArtPiece/ArtPiece.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtPiece : MonoBehaviour
+{
+    public GameObject currentArt;
+    
+    public void ChangePiece(GameObject piece)
+    {
+        if(currentArt != null) Destroy(currentArt);
+
+        currentArt = Instantiate(piece, transform);
+        currentArt.transform.localPosition = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
